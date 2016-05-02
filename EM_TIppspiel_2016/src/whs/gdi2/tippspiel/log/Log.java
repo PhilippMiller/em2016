641bd73bfadd4519f4f5e1b28a633d369511032c
Mario Kellner <Mario.Kellner@studmail.w-hs.de>
2016-05-02 15:58:06 +0200

    Logger updated.
    Possible LOG Levels:
    
    	INFO ,
    	STOP,
    	DEBUG,
    	MYSQL,
    	MYSQL_ERROR, 
    	ERROR,
    	CRITICAL,

--- EM_TIppspiel_2016/src/whs/gdi2/tippspiel/log/Log.java
+++ EM_TIppspiel_2016/src/whs/gdi2/tippspiel/log/Log.java
@@ -1,13 +1,12 @@
 package whs.gdi2.tippspiel.log;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import whs.gdi2.tippspiel.*;
 
-import com.mysql.fabric.xmlrpc.base.Data;
 
 /**
  * 
@@ -21,41 +20,73 @@ import com.mysql.fabric.xmlrpc.base.Data;
  */
 
 public class Log {
+	protected static String logFolder = Config.getLogDir();
 	
-	public static void main(String [] args) {
-		System.out.println(date());
+	public static String getLogFolder() {
+		return logFolder;
 	}
 
-	public static void add(String logZeile, LogLevel logLevel) {
-		boolean exist = true;
-		String logFileName = Variables.logFilePath + Datum.tagDatum + Variables.logFileType;
-		File logFile = new File(logFileName);
+	public static void info(String message) {
+		Log.log(LogLevel.INFO, message);
+	}
 
+	public static void debug(String message) {
+		Log.log(LogLevel.DEBUG, message);
+	}
+	
+	public static void error(String message) {
+		Log.log(LogLevel.ERROR, message);
+		
+	}
+	public static void critical(String message) {
+		Log.log(LogLevel.CRITICAL, message);
+		Log.exit(LogLevel.CRITICAL);
+	}
+
+	public static void stop(String message) {
+		Log.log(LogLevel.STOP, message);
+		Log.exit(LogLevel.STOP);
+	}
+	
+	public static void mysql(String message) {
+		Log.log(LogLevel.MYSQL, message);
+	}
+	
+	public static void mysqlError(String message) {
+		Log.log(LogLevel.MYSQL_ERROR, message);
+	}
+	
+	public static void log(LogLevel level, String message) {
+		String logFileName = "log_" + Log.date() + ".log";
+		
+		File logFolder = new File(Config.getLogDir());
+		
+		if (!logFolder.exists()) {
+		    try{
+		    	logFolder.mkdirs();
+		    } 
+		    catch(SecurityException se){
+		    	System.out.println(se.getMessage());
+		    }
+		}
+		
 		FileWriter fw = null;
 		BufferedWriter bw = null;
-
+		File logFile = new File(Log.getLogFolder() + "/" + logFileName);
+		
+		
 		try {
 
 			if (!logFile.exists()) {
 				if (logFile.createNewFile()) {
-					Log.add("LogFile '" + logFileName + "' erstellt!", logLevel);
-				} else {
-					System.err.println("Konnte LogFile '" + logFileName + "' nicht erstellt!");
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					System.exit(0);
-
+					Log.info("LogFile '" + logFileName + "' created!");
 				}
 			}
 
-			fw = new FileWriter(logFile, true);
-			bw = new BufferedWriter(fw);
-			bw.write("[" + logLevel.getType() + "]\t[" + Datum.tagUndZeitDatum + "]  " + logZeile
+			bw = new BufferedWriter(new FileWriter(logFile, true));
+			bw.write("[" + level.name() + "][" + (new Date()).toString() + "]  " + message
 					+ System.lineSeparator());
+			
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -68,19 +99,6 @@ public class Log {
 				}
 			}
 		}
-
-		switch (logLevel.getValue()) {
-		case 0:
-			System.err.println(
-					"Programm angehalten!" + System.lineSeparator() + "Erfasster Typ: '" + logLevel.getType() + "'!");
-			break;
-		case 4:
-			exit(4);
-			break;
-		case 5:
-			exit(5);
-			break;
-		}
 	}
 	
 	private static String date() {
@@ -88,25 +106,16 @@ public class Log {
 		SimpleDateFormat df = new SimpleDateFormat("dd-MM-YYYY");
 		return df.format(date);
 	}
-
-	private static void exit(int value) {
-		// EVTL DATEN SPEICHERN?! MYSQL ODER SO?
-		switch (value) {
-		case 4:
-			System.err.println("Critischer Fehler aufgetreten!" + System.lineSeparator() + "Programm wird beendet...");
+	
+	private static void exit(LogLevel level) {
+		switch (level) {
+		case CRITICAL:
+			Log.log(LogLevel.CRITICAL,"Critical error occurs!" + System.lineSeparator() + "Application will be terminated...");
 			break;
-		case 5:
-			System.err.println("Gelpanter Stopp ausgeführt!" + System.lineSeparator() + "Programm wird beendet...");
+		case STOP:
+			Log.log(LogLevel.STOP, "Received stop signal!" + System.lineSeparator() + "Application will be terminated...");
 			break;
 		}
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
 		System.exit(0);
-	}
-
+}
 }
