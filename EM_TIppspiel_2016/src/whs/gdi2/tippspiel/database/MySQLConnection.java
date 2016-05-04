1f002bc8b2aef68a748d4cd91c656f420ebb4793
Mario Kellner <Mario.Kellner@studmail.w-hs.de>
2016-05-04 11:25:42 +0200

    added switchDatabase

--- EM_TIppspiel_2016/src/whs/gdi2/tippspiel/database/MySQLConnection.java
+++ EM_TIppspiel_2016/src/whs/gdi2/tippspiel/database/MySQLConnection.java
@@ -17,7 +17,6 @@ public class MySQLConnection {
 	protected String databasePassword;
 	protected String database;
 	
-	
 	private MySQLConnection() throws ClassNotFoundException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
