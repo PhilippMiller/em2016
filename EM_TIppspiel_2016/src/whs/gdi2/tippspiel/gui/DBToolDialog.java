149345e36f06d45be994b3eac8cba5113e9f045f
ja32759 <jan-markus.momper@studmail.w-hs.de>
2016-06-02 13:13:24 +0200

    Finished ?

--- EM_TIppspiel_2016/src/whs/gdi2/tippspiel/gui/DBToolDialog.java
+++ EM_TIppspiel_2016/src/whs/gdi2/tippspiel/gui/DBToolDialog.java
@@ -36,11 +36,11 @@ import javax.swing.UIManager;
 /**
  * Database management tool for the instance of testdatabase.
  * 
- * This tool can recreate the the database and fill it with testdata.
- * It can also show some informations over the testdatabase.
+ * This tool can recreate the the database and fill it with testdata. It can
+ * also show some informations over the testdatabase.
  * 
- * It is only usable with the testdatabase because testdatabase instances has always more rights
- * than instances which holds livedatabases.
+ * It is only usable with the testdatabase because testdatabase instances has
+ * always more rights than instances which holds livedatabases.
  * 
  * @since 1.0
  * @version 1.2
@@ -71,7 +71,7 @@ public class DBToolDialog extends JDialog {
 	private JLabel lblTabellenExistieren;
 	private JLabel lblDatenbankExistiert;
 	private JFileChooser fc;
-	
+
 	/**
 	 * Create the dialog.
 	 */
@@ -79,19 +79,19 @@ public class DBToolDialog extends JDialog {
 		super(parent);
 
 		classContext = this;
-		
+
 		setTitle("Tippspiel Admin - Tool | Verwaltung der Testdatenbank");
 		setBackground(Config.getGuiColor());
 		setModal(true);
 		setSize(450, 300);
-		
-        Dimension windowSize = this.getSize();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation(screenSize.width/2 - windowSize.width/2, screenSize.height/2 - windowSize.height/2);
-		
+
+		Dimension windowSize = this.getSize();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(screenSize.width / 2 - windowSize.width / 2, screenSize.height / 2 - windowSize.height / 2);
+
 		InitializeGui();
 		InitializeEvents();
-		
+
 		reload();
 	}
 
@@ -101,80 +101,79 @@ public class DBToolDialog extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		fc = new JFileChooser();
-		
+
 		panel_1 = new JPanel();
 		panel_1.setBounds(5, 5, 168, 215);
 		panel_1.setBorder(new LineBorder(UIManager.getColor("ComboBox.disabledForeground"), 2));
 		panel_1.setBackground(UIManager.getColor("Button.light"));
 		contentPanel.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		lblNewLabel = new JLabel("Datenbankstatus");
 		lblNewLabel.setBounds(7, 7, 101, 14);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		panel_1.add(lblNewLabel);
-		
+
 		lblHost = new JLabel("Host:");
 		lblHost.setBounds(7, 32, 36, 14);
 		panel_1.add(lblHost);
-		
+
 		lblHostPlaceholder = new JLabel("host placeholder");
 		lblHostPlaceholder.setBounds(7, 50, 151, 14);
 		panel_1.add(lblHostPlaceholder);
-		
+
 		lblUser = new JLabel("User:");
 		lblUser.setBounds(7, 72, 36, 14);
 		panel_1.add(lblUser);
-		
+
 		lblUserPlaceholder = new JLabel("user placeholder");
 		lblUserPlaceholder.setBounds(7, 90, 123, 14);
 		panel_1.add(lblUserPlaceholder);
-		
+
 		lblDatabase = new JLabel("Datenbank:");
 		lblDatabase.setBounds(7, 115, 86, 14);
 		panel_1.add(lblDatabase);
-		
+
 		lblDbPlaceholder = new JLabel("db placeholder");
 		lblDbPlaceholder.setBounds(7, 133, 76, 14);
 		panel_1.add(lblDbPlaceholder);
-		
+
 		lblDBExistiert = new JLabel("Datenbank existiert: ");
 		lblDBExistiert.setBounds(7, 165, 123, 14);
 		panel_1.add(lblDBExistiert);
-		
+
 		lblDatenbankExistiert = new JLabel("ja");
 		lblDatenbankExistiert.setBounds(131, 165, 48, 14);
 		panel_1.add(lblDatenbankExistiert);
-		
+
 		lblTabellenExistieren = new JLabel("Tabellen existieren:");
 		lblTabellenExistieren.setBounds(7, 190, 123, 14);
 		panel_1.add(lblTabellenExistieren);
-		
+
 		lblTableplaceholder = new JLabel("tableplaceholder");
 		lblTableplaceholder.setBounds(131, 190, 101, 14);
 		panel_1.add(lblTableplaceholder);
-		
+
 		panel = new JPanel();
 		panel.setBounds(173, 0, 256, 220);
 		panel.setBackground(Config.getGuiColor());
 		contentPanel.add(panel);
 		panel.setLayout(null);
-		
+
 		btnDeleteTables = new JButton("Lösche Tabellen");
 		btnDeleteTables.setBounds(10, 87, 236, 27);
 		panel.add(btnDeleteTables);
-		
+
 		btnDeleteTables.setFont(new Font(Config.getFont(), Font.PLAIN, 15));
 		btnDeleteTables.setBackground(Config.getGuiColor());
-	
+
 		btnCreateTables = new JButton("Erstelle Tabellen");
 		btnCreateTables.setBounds(10, 125, 236, 27);
 		panel.add(btnCreateTables);
 		btnCreateTables.setFont(new Font(Config.getFont(), Font.PLAIN, 15));
 		btnCreateTables.setBackground(Config.getGuiColor());
-	
 
 		button = new JButton("Testdaten Einfügen");
 		button.setBounds(10, 163, 236, 27);
@@ -182,19 +181,19 @@ public class DBToolDialog extends JDialog {
 
 		button.setFont(new Font("Calibri Light", Font.PLAIN, 15));
 		button.setBackground(Color.WHITE);
-		
+
 		btnDatenbankNeuAnlegen = new JButton("Datenbank neu anlegen");
 		btnDatenbankNeuAnlegen.setBounds(10, 49, 236, 27);
 
 		btnDatenbankNeuAnlegen.setFont(new Font("Calibri Light", Font.PLAIN, 15));
 		btnDatenbankNeuAnlegen.setBackground(Color.WHITE);
 		panel.add(btnDatenbankNeuAnlegen);
-		
+
 		buttonPane = new JPanel();
 		buttonPane.setBackground(Config.getGuiColor());
 		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		getContentPane().add(buttonPane, BorderLayout.SOUTH);
-		
+
 		cancelButton = new JButton("Schlie\u00DFen");
 
 		cancelButton.setBackground(Config.getGuiColor());
@@ -204,50 +203,53 @@ public class DBToolDialog extends JDialog {
 	}
 	
 	public void InitializeEvents() {
+		
+		String[] options = {"Interne Testdaten benutzen", "Externe Testdaten benutzen"};
+		
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-Log.info("Menue item 'Testdaten Einfügen' clicked.");
-				
-				if(DatabaseManagement.addTestData(Main.mainConnection)) {
-					int answer = JOptionPane.showConfirmDialog(
-						    classContext,
-						    "Möchten Sie für die Tabelle Spiele Testdaten einlesen?",
-						    "Testdaten benutzen",
-						    JOptionPane.YES_NO_OPTION);
-					
-					if(answer == JOptionPane.YES_OPTION) {
-						DatabaseManagement.importGameData(Main.mainConnection, new InputStreamReader(DBToolDialog.class.getResourceAsStream("/whs/gdi2/tippspiel/data/tipps.txt")));
-					}
-					else {
+				Log.info("Menue item 'Testdaten Einfügen' clicked.");
 
-				        int returnVal = fc.showOpenDialog(classContext);
-				        
-				        if (returnVal == JFileChooser.APPROVE_OPTION) {
-				            File file = fc.getSelectedFile();
+				if (DatabaseManagement.addTestData(Main.mainConnection)) {
+					int answer = JOptionPane.showOptionDialog(classContext, "Wie möchten Sie die Tastdaten einlesen?",
+							"Testdaten Einlesen", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+					        null, options, options[0]);
+
+					if (answer == JOptionPane.YES_OPTION) {
+						DatabaseManagement.importGameData(Main.mainConnection, new InputStreamReader(
+								DBToolDialog.class.getResourceAsStream("/whs/gdi2/tippspiel/data/tipps.txt")));
+					} else {
+
+						int returnVal = fc.showOpenDialog(classContext);
+
+						if (returnVal == JFileChooser.APPROVE_OPTION) {
+							File file = fc.getSelectedFile();
 
 							try {
-								DatabaseManagement.importGameData(Main.mainConnection, new InputStreamReader(new FileInputStream(file)));
+								DatabaseManagement.importGameData(Main.mainConnection,
+										new InputStreamReader(new FileInputStream(file)));
 							} catch (FileNotFoundException e1) {
 								Log.error("Cannot open " + file.getAbsolutePath());
 								JOptionPane.showMessageDialog(classContext, "Spiele wurden nicht importiert");
-					         
+
 							}
-						
-				        } else {
-				        	JOptionPane.showMessageDialog(classContext, "Spiele wurden nicht importiert");
-				            Log.info("Spiele wurden nicht importiert");
-				        }
+
+						} else {
+							JOptionPane.showMessageDialog(classContext, "Spiele wurden nicht importiert");
+							Log.info("Spiele wurden nicht importiert");
+						}
 					}
-					JOptionPane.showMessageDialog(classContext, "Testdaten wurden in Datenbank geschrieben.", "Testdaten importieren", JOptionPane.PLAIN_MESSAGE);
-				}
-				else {
-					JOptionPane.showMessageDialog(classContext, "Testdaten konnten nicht importiert werden.", "Testdaten importieren", JOptionPane.WARNING_MESSAGE);
+					JOptionPane.showMessageDialog(classContext, "Testdaten wurden in Datenbank geschrieben.",
+							"Testdaten importieren", JOptionPane.PLAIN_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(classContext, "Testdaten konnten nicht importiert werden.",
+							"Testdaten importieren", JOptionPane.WARNING_MESSAGE);
 				}
 
 				reload();
 			}
 		});
-		
+
 		btnDatenbankNeuAnlegen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -255,62 +257,64 @@ Log.info("Menue item 'Testdaten Einfügen' clicked.");
 				reload();
 			}
 		});
-		
+
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				DBToolDialog.this.dispose();
 			}
 		});
-		
+
 		btnDeleteTables.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Log.info("Menue item 'Lösche Tabellen' clicked.");
-				if(DatabaseManagement.dropTables(Main.mainConnection)) {
-					JOptionPane.showMessageDialog(classContext,"Tabellen wurden gelöscht.", "Tabellen löschen", JOptionPane.PLAIN_MESSAGE);
-				}
-				else {
-					JOptionPane.showMessageDialog(classContext,"Tabellen konnten nicht gelöscht werden.", "Tabellen löschen", JOptionPane.WARNING_MESSAGE);
-					
+				if (DatabaseManagement.dropTables(Main.mainConnection)) {
+					JOptionPane.showMessageDialog(classContext, "Tabellen wurden gelöscht.", "Tabellen löschen",
+							JOptionPane.PLAIN_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(classContext, "Tabellen konnten nicht gelöscht werden.",
+							"Tabellen löschen", JOptionPane.WARNING_MESSAGE);
+
 				}
 				reload();
 			}
 		});
-		
+
 		btnCreateTables.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
 				Log.info("Menue item 'Erstelle Tabellen' clicked.");
-				if(!DatabaseManagement.createTables(Main.mainConnection)) {
-					JOptionPane.showMessageDialog(classContext, "Tabellen konnten nicht erstellt werden.", "Tabellen erstellen", JOptionPane.WARNING_MESSAGE);
-				}
-				else {
+				if (!DatabaseManagement.createTables(Main.mainConnection)) {
+					JOptionPane.showMessageDialog(classContext, "Tabellen konnten nicht erstellt werden.",
+							"Tabellen erstellen", JOptionPane.WARNING_MESSAGE);
+				} else {
 
-					JOptionPane.showMessageDialog(classContext, "Tabellen wurden erfolgreich angelegt.", "Tabellen erstellen", JOptionPane.PLAIN_MESSAGE);
+					JOptionPane.showMessageDialog(classContext, "Tabellen wurden erfolgreich angelegt.",
+							"Tabellen erstellen", JOptionPane.PLAIN_MESSAGE);
 				}
 				reload();
 			}
 		});
 	}
-	
+
 	public void reload() {
 		lblHostPlaceholder.setText(Config.getDBIp_offline());
 		lblUserPlaceholder.setText(Config.getDBUser_offline());
 		lblDbPlaceholder.setText(Config.getDB_offline());
-		
-		
+
 		lblTableplaceholder.setText("nein");
 		lblDatenbankExistiert.setText("nein");
-		
-		if(MySQLConnection.testConnection(Config.getDBIp_offline(), Config.getDBUser_offline(), Config.getDBPass_offline(), "")) {
-			if(MySQLConnection.testConnection(Config.getDBIp_offline(), Config.getDBUser_offline(), Config.getDBPass_offline(), Config.getDB_offline())) {
+
+		if (MySQLConnection.testConnection(Config.getDBIp_offline(), Config.getDBUser_offline(),
+				Config.getDBPass_offline(), "")) {
+			if (MySQLConnection.testConnection(Config.getDBIp_offline(), Config.getDBUser_offline(),
+					Config.getDBPass_offline(), Config.getDB_offline())) {
 				lblDatenbankExistiert.setText("ja");
 				try {
 					Statement stmt = Main.mainConnection.getConnection().createStatement();
 					stmt.executeQuery("SELECT * FROM spiele LIMIT 1");
 					lblDatenbankExistiert.setText("ja");
 					lblTableplaceholder.setText("ja");
-				}
-				catch(Exception e) {
+				} catch (Exception e) {
 					// tables doesnt exists;
 				}
 			}
