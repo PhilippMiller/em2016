3689841daa92d7e1bfdb1d5174a8fa24944a6724
Mario Kellner <Mario.Kellner@studmail.w-hs.de>
2016-06-09 00:10:14 +0200

    btn Speichern

--- EM_TIppspiel_2016/src/whs/gdi2/tippspiel/gui/ErgebnisEingabeFrame.java
+++ EM_TIppspiel_2016/src/whs/gdi2/tippspiel/gui/ErgebnisEingabeFrame.java
@@ -64,9 +64,9 @@ public class ErgebnisEingabeFrame extends JDialog {
 	protected JRadioButton rdbtnErfassteSpieleBearbeiten;
 	private JComboBox<Match> comboBox;
 	private JButton button;
-	private JCheckBox checkBox_1;
+	protected JCheckBox checkBox_1;
 	protected JCheckBox checkBox;
-
+	protected Match currentMatch;
 	/**
 	 * Create the dialog.
 	 */
@@ -335,9 +335,19 @@ public class ErgebnisEingabeFrame extends JDialog {
 		/*
 		 * LISTENER
 		 */
+		btnSpeichern.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(currentMatch != null) {
+					System.out.println("works");
+					reload();
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Du bearbeitest kein Spiel. Bitte wähle zuerst ein Spiel aus.", "Information", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
 		
 		button.addActionListener(new ActionListener() {
-			private Match currentMatch;
 
 			public void actionPerformed(ActionEvent arg0) {
 				currentMatch = (Match) comboBox.getSelectedItem();
@@ -512,6 +522,8 @@ public class ErgebnisEingabeFrame extends JDialog {
 	}
 	
 	public void clear() {
+		currentMatch = null;
+		
 		textField.setText("0");
 		textField_1.setText("0");
 		textField_2.setText("0");
